5d667e2d5a683bf5ea72ebb1f5ac033bca9b2923
Ari Timonen <arit@fastmail.com>
2023-10-02 10:51:09 +0300

    Update frontend and back to basic version

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,7 +7,9 @@ const cors = require('kcors',);
 
 const appId = process.env.APPID || '';
 const mapURI = process.env.MAP_ENDPOINT || 'http://api.openweathermap.org/data/2.5';
+const geoURI = 'http://api.openweathermap.org/geo/1.0';
 const targetCity = process.env.TARGET_CITY || 'Helsinki,fi';
+const cityLimit = 1;
 
 const port = process.env.PORT || 9000;
 
@@ -16,17 +18,41 @@ const app = new Koa();
 app.use(cors(),);
 
 const fetchWeather = async () => {
-  const endpoint = `${mapURI}/weather?q=${targetCity}&appid=${appId}&`;
+  const endpoint = `${mapURI}/weather?q=${targetCity}&appid=${appId}`;
+  const response = await fetch(endpoint,);
+
+  return response ? response.json() : {};
+};
+
+const fetchCityCoords = async (lat, long,) => {
+  const endpoint = `${geoURI}/reverse?lat=${lat}&lon=${long}&limit=${cityLimit}&appid=${appId}`;
+  const response = await fetch(endpoint,);
+
+  return response && await response.json()[0] ? [response.json()[0].lat, response.json()[0].lon] : [60.1695, 24.9355];
+};
+
+const fetchForecast = async (lat, long,) => {
+  let endpoint;
+
+  // Check and validate location data
+  if (lat && long && /^[0-9]{1,3}\.[0-9]{1,15}$/.test(lat,) && /^[0-9]{1,3}\.[0-9]{1,15}$/.test(long,)) {
+    // [cityLat, cityLong] = await fetchCityCoords(lat, long,);
+    endpoint = `${mapURI}/forecast?lat=${lat}&lon=${long}&units=metric&appid=${appId}`;
+  } else {
+    endpoint = `${mapURI}/forecast?q=${targetCity}&appid=${appId}`;
+  }
   const response = await fetch(endpoint,);
 
   return response ? response.json() : {};
 };
 
 router.get('/api/weather', async ctx => {
-  const weatherData = await fetchWeather();
+  const { lat, long, } = ctx.query;
+  const weatherData = await fetchForecast(lat, long,);
 
   ctx.type = 'application/json; charset=utf-8';
-  ctx.body = weatherData.weather ? weatherData.weather[0] : {};
+  // ctx.body = weatherData.weather ? weatherData.weather[0] : {};
+  ctx.body = weatherData;
 },);
 
 app.use(router.routes(),);
--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Accordion from 'react-bootstrap/Accordion';
+import Stack from 'react-bootstrap/Stack';
+
 const baseURL = process.env.ENDPOINT;
 
-const getWeatherFromApi = async () => {
+const getWeatherFromApi = async (lat, long) => {
+  let response;
+
   try {
-    const response = await fetch(`${baseURL}/weather`);
+    if (lat && long) {
+      response = await fetch(`${baseURL}/weather?lat=${lat}&long=${long}`);
+    } else {
+      response = await fetch(`${baseURL}/weather`);
+    }
     return response.json();
   } catch (error) {
     console.error(error);
@@ -19,24 +31,76 @@ class Weather extends React.Component {
     super(props);
 
     this.state = {
-      icon: '',
+      weather: {},
     };
   }
 
   async componentDidMount() {
-    const weather = await getWeatherFromApi();
-    this.setState({
-      icon: weather.icon.slice(0, -1),
+    let crd;
+
+    // This could be wrapped in async+await structure
+    navigator.geolocation.getCurrentPosition(async (pos) => {
+      crd = pos.coords;
+
+      const weather = await getWeatherFromApi(crd.latitude, crd.longitude);
+
+      // Fetch the weather data from backend
+      this.setState({
+        weather,
+      });
+
+      console.log("Your current position is:");
+      console.log(`Latitude : ${crd.latitude}`);
+      console.log(`Longitude: ${crd.longitude}`);
+      console.log(`More or less ${crd.accuracy} meters.`);
+    },
+    async (err) => {
+      const weather = await getWeatherFromApi(crd.latitude, crd.longitude);
+      // Fetch the weather data from backend
+      this.setState({
+        weather,
+      });
+
+      console.warn(`ERROR(${err.code}): ${err.message}`);
+    },
+    {
+      enableHighAccuracy: true,
+      timeout: 60000,
+      maximumAge: 0,
     });
   }
 
   render() {
-    const { icon } = this.state;
+    const { weather } = this.state;
 
     return (
-      <div className="icon">
-        { icon && <img src={`/img/${icon}.svg`} alt="Weather icon" /> }
-      </div>
+      <Stack gap={3}>
+        { weather.list && weather.list.slice(0, 5).map((e, i) => (
+          <div className="p-2">
+            <h3>
+              { weather.city.name }
+              &nbsp;-&nbsp;
+              { e.dt_txt }
+            </h3>
+            { e.weather[0].main }
+            &nbsp;
+            { <img src={`/img/${e.weather[0].icon.slice(0, -1)}.svg`} alt="Weather icon" width="15" height="15" /> }
+            <br />
+            Temperature:&nbsp;
+            { e.main.temp }
+            &nbsp;
+            (
+            { e.main.temp_min }
+            -
+            { e.main.temp_max }
+            )
+            <br />
+            Wind:&nbsp;
+            { e.wind.speed }
+            &nbsp; m/s
+          </div>
+        )) }
+      </Stack>
     );
   }
 }
