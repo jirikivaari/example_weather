2a03c6202931516a0de76f8d08f59ae2e6a0f6c4
Ari Timonen <arit@fastmail.com>
2023-10-09 05:04:32 +0300

    scripts: Fix local execution

--- scripts/local.sh
+++ scripts/local.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
 APP_ROOT=$(dirname "$0")/..
+APP_ROOT_ABS=$(realpath "$APP_ROOT")
 
-cd "$APP_ROOT/frontend" ||  { echo "Cannot find application"; exit 1; }
-cd "$APP_ROOT/backend" ||  { echo "Cannot find application"; exit 1; }
+test -d "$APP_ROOT/frontend" ||  { echo "Cannot find frontend"; exit 1; }
+test -d "$APP_ROOT/backend" ||  { echo "Cannot find backend"; exit 1; }
 [[ -n "$APPID" ]] || { echo "Please set ENV variable APPID to API key."; exit 2; }
 
 # Start backend server.
-cd "$APP_ROOT/backend"
+cd "$APP_ROOT_ABS/backend"
 npm install ||  { echo "Cannot install NPM packages."; exit 1; }
-npm run start &
+npm run dev &
 
 sleep 5
 
 # Start frontend server.
-cd "$APP_ROOT/frontend"
+cd "$APP_ROOT_ABS/frontend"
 npm install ||  { echo "Cannot install NPM packages."; exit 1; }
 npm run start
